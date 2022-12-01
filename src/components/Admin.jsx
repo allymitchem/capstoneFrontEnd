cb9603bd6b647c1d3d9ecf5b7037f8e2b0182b33
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-30 17:17:31 -0700

    user's id, username, and email(all users) displaying in admin view

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { adminUserData } from "../api/users";
 
 const Admin =()=> {
     
+    const [userList, setUserList]= useState([])
+    useEffect(()=>{
+        async function getUsers (){
+            const allUsers = await adminUserData()
+            console.log(allUsers, "admin comp")
+            setUserList(allUsers)
 
-    async function getUsers (){
-        const allUsers = await adminUserData()
-        console.log(allUsers, "admin comp")
-    }
-    getUsers()
+        }
+        getUsers()
+    }, [])
 
     return (
         <div>
-            I am admin
+            {userList.map((user) => {
+                return (
+                    <div className="all_users_list" key={`userList-id${user.id}`}>
+                        <div><b>ID: </b>{user.id}</div>
+                        <div><b>Username: </b>{user.username}</div>
+                        <div><b>Email: </b> {user.email}</div>
+                    </div>
+                )
+            })
+
+            }
         </div>
     )
 
