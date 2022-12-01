90c9d2e312953e41b7260b843f827d61e9a17ad6
Kaylan Thompson <kaylanthompson@icloud.com>
2022-12-01 12:57:44 -0600

    delete from cart for logged in user working.

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { deleteBookFromCart } from '../api/carts';
+import Book from './Book';
 
 
 
-const CartList = ({elem, }) => {
+const CartList = ({elem, cart, user, setCart}) => {
+    const indexInCart = cart.items.findIndex(book => book.itemId == elem.itemId)
+
+
+
+    async function handleDelete(event) {
+        event.preventDefault()
+        if(cart && user.id) {
+            const unwantedBook = (cart.items[indexInCart].id)
+            const deletedBook = await deleteBookFromCart(unwantedBook)
+            const newCart = {...cart}
+            newCart.items = cart.items.filter(book => book.itemId !== deletedBook.itemId)
+            setCart(newCart)   
+
+}
+
+    }
    
 
     return (
@@ -10,7 +28,7 @@ const CartList = ({elem, }) => {
             <img src={elem.imageURL}/>
             <p>${elem.price/100}</p>
             <p>Quantity: {elem.quantity}</p>
-            <button>Remove From Cart</button>         
+            <button onClick={handleDelete}>Remove From Cart</button>         
         </div>
         
     )
