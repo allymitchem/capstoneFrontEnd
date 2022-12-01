cc43fec58bd60fc3e6f388757503e761da398b62
Kaylan Thompson <kaylanthompson@icloud.com>
2022-12-01 14:22:16 -0600

    working on the quantity function and update function

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,53 @@
+import React, { useReducer, useState } from 'react';
+import { deleteBookFromCart, saveLocalCart } from '../api/carts';
+import Book from './Book';
+
+
+
+const CartItem = ({elem, cart, user, setCart}) => {
+    const [quantity, setQuantity] = useState(elem.quantity)
+    console.log(quantity, "this is the quantity state")
+
+    const indexInCart = cart.items.findIndex(book => book.itemId == elem.itemId)
+    // console.log(cart.items)
+    // console.log(elem, "element")
+
+
+
+    async function handleDelete(event) {
+        event.preventDefault()
+        const unwantedBook = (cart.items[indexInCart].itemId)
+        if(cart && user.id) {
+            const deletedBook = await deleteBookFromCart(unwantedBook)
+            const newCart = {...cart}
+            newCart.items = cart.items.filter(book => book.itemId !== deletedBook.itemId)
+            setCart(newCart)
+        } else {            
+            const newCart = {...cart}
+            newCart.items = cart.items.filter(book => book.itemId !== unwantedBook)
+            setCart(newCart)
+            saveLocalCart(newCart)
+        }
+    }
+
+    async function handleOnChange (event) {
+        event.preventDefault()
+        
+        
+    }
+   
+
+    return (
+        <div className="cart_list">          
+            <img src={elem.imageURL}/>
+            <p>${elem.price/100}</p>
+            <input type='number' value={quantity} onChange={(elem) => setQuantity(elem.target.value)}/>
+            {/* <p>Quantity: {elem.quantity}</p> */}
+            <button onClick={handleDelete}>Remove From Cart</button> 
+                    
+        </div>
+        
+    )
+}
+
+export default CartItem
--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BookList from './BookList';
-import {CartList} from './'
+import {CartItem} from './'
 
 const CartPage = ({cart, setCart, user}) => {
 
@@ -10,7 +10,7 @@ const CartPage = ({cart, setCart, user}) => {
     return (
         <div className="cart_page">
             {cart && cart.items.length ?
-            cart.items.map((elem, index) => <CartList key={`elem_${index}`} elem={elem} cart={cart} setCart={setCart} user={user}/>)
+            cart.items.map((elem, index) => <CartItem key={`elem_${index}`} elem={elem} cart={cart} setCart={setCart} user={user}/>)
             
             
              : "Life is full and overflowing with the new. But it is necessary to empty out the old to make room for the new to enter. - Eileen Caddy"}
