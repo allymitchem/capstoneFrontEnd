6c7e47b8663773e923098845477e9faff668a2df
Kaylan Thompson <kaylanthompson@icloud.com>
2022-12-02 09:05:03 -0600

    fixed timing issue of cart loading

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,45 +1,56 @@
-import React from 'react';
-import BookList from './BookList';
+import React from "react"
+import BookList from "./BookList"
 
-import {CartItem} from './'
-import { patchBook } from '../api/books';
-import { getCart, markCartInactive } from '../api/carts';
+import { CartItem } from "./"
+import { patchBook } from "../api/books"
+import { getCart, markCartInactive } from "../api/carts"
 
-const CartPage = ({cart, setCart, user}) => {
+const CartPage = ({ cart, setCart, user }) => {
+  async function handleCheckout(event) {
+    event.preventDefault()
+    //this does not checkout a guest user yet
 
-
-    async function handleCheckout(event) {
-        event.preventDefault()
-        //this does not checkout a guest user yet
-
-        //check that we have enough stock
-        const newStock = cart.items.map(elem => {return {...elem, numInStock: elem.numInStock - elem.quantity}})
-        if (newStock.every((elem) => elem.numInStock > 0)) {
-            //remove the cart quantity from the stock
-            for (const book of newStock) {
-                await patchBook(book.itemId,{numInStock: book.numInStock})
-            }
-            //mark the cart as inactive
-            const deadCart = await markCartInactive(cart.id)
-            //get a new cart
-            const cartData = await getCart(user.id)
-            setCart(cartData)
-        } else {
-            alert("There is not enough stock")
-        }
+    //check that we have enough stock
+    const newStock = cart.items.map((elem) => {
+      return { ...elem, numInStock: elem.numInStock - elem.quantity }
+    })
+    if (newStock.every((elem) => elem.numInStock > 0)) {
+      //remove the cart quantity from the stock
+      for (const book of newStock) {
+        await patchBook(book.itemId, { numInStock: book.numInStock })
+      }
+      //mark the cart as inactive
+      const deadCart = await markCartInactive(cart.id)
+      //get a new cart
+      const cartData = await getCart(user.id)
+      setCart(cartData)
+    } else {
+      alert("There is not enough stock")
     }
+  }
 
-    return (
-        <div className="cart_page">
-            {cart && cart.items.length ?
-            cart.items.map((elem) => <CartItem key={`elem_${elem.userId}`} elem={elem} cart={cart} setCart={setCart} user={user}/>)
-            
-            
-             : "Life is full and overflowing with the new. But it is necessary to empty out the old to make room for the new to enter. - Eileen Caddy"}
-            <p>Subtotal: ${cart.items.reduce((sum, elem) => sum += elem.price * elem.quantity,0)/100}</p>
-            <button onClick={handleCheckout}>Checkout</button>
-        </div>
-    )   
+  return (
+    <div className="cart_page">
+      {cart && cart.items.length
+        ? cart.items.map((elem) => {
+            console.log(elem)
+            return (
+              <CartItem
+                key={`cart_page_elem_${elem.itemId}`}
+                elem={elem}
+                cart={cart}
+                setCart={setCart}
+                user={user}
+              />
+            )
+          })
+        : "Life is full and overflowing with the new. But it is necessary to empty out the old to make room for the new to enter. - Eileen Caddy"}
+      <p>
+        Subtotal: ${cart.items.reduce((sum, elem) => (sum += elem.price * elem.quantity), 0) / 100}
+      </p>
+      <button onClick={handleCheckout}>Checkout</button>
+    </div>
+  )
 }
 
 export default CartPage
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,60 +1,72 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {Navbar, ProductsPage, Register, BookPage, Admin, CartPage} from "./"
+import { Navbar, ProductsPage, Register, BookPage, Admin, CartPage } from "./"
 import { getCart } from "../api/carts"
 import { getCurrentUser } from "../api/users"
 
 const Main = () => {
-    const [user, setUser] = useState({id: 0, username: "guest"})
-    const [cart, setCart] = useState({id: 0, userId: 0, creatorName: "guest",items: []})
+  const [user, setUser] = useState({ id: 0, username: "guest" })
+  const [cart, setCart] = useState({ id: 0, userId: 0, creatorName: "guest", items: [] })
+  const [count, setCount] = useState(0)
 
-    useEffect(()=>{
-        //check for token and check that the token belongs to a user
-        const token = localStorage.getItem("token")
-        if (token){
-            async function callGetCurrentUser() {
-                const currentUser = await getCurrentUser()
-                setUser(currentUser)
-            }
-            callGetCurrentUser()
+  useEffect(() => {
+    //check for token and check that the token belongs to a user
+    const token = localStorage.getItem("token")
+    if (token) {
+      async function callGetCurrentUser() {
+        const currentUser = await getCurrentUser()
+
+        if (currentUser) {
+          setUser(currentUser)
+          // setCount(count + 1)
         }
-    }, [])
-    
-    useEffect(() => {
-        //if a user is logged in then get their cart from DB
-        if (user.id) {
-            async function callGetCart() {
-                const cartData = await getCart(user.id)
-                setCart(cartData)
-            }
-            callGetCart()
+      }
+      callGetCurrentUser()
+    }
+  }, [])
+
+  useEffect(() => {
+    //if a user is logged in then get their cart from DB
+
+    setTimeout(() => {
+      if (user.id) {
+        async function callGetCart() {
+          const cartData = await getCart(user.id)
+          if (cartData) {
+            setCart(cartData)
+          }
+        }
+        callGetCart()
+      } else {
+        //since the user is not logged in check if they have a cart in local storage
+        const localCart = localStorage.getItem("cart")
+        if (localCart) {
+          setCart(JSON.parse(localCart))
         } else {
-            //since the user is not logged in check if they have a cart in local storage
-            const localCart = localStorage.getItem('cart')
-            if (localCart) {
-                setCart(JSON.parse(localCart))
-            } else {
-                setCart({id: 0, userId: 0, creatorName: "guest",items: []})
-            }
+          localCart.removeItem("cart")
+          setCart({ id: 0, userId: 0, creatorName: "guest", items: [] })
         }
-    },[user])
+      }
+    }, 250)
+  }, [user])
 
-    
-
-    return (
-        <div id="main">
-            <BrowserRouter>
-            <Navbar user={user} setUser={setUser} cart={cart}/>
-                <Routes>
-                        <Route path='products' element={<ProductsPage user={user} cart={cart} setCart={setCart}/>} />
-                        <Route path='register' element={<Register />} />
-                        <Route path=':itemId' element={<BookPage user={user} cart={cart} setCart={setCart}/>} />
-                        <Route path='admin' element={<Admin/>}/>
-                        <Route path='cart' element={<CartPage cart={cart} setCart={setCart} user={user}/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div id="main">
+      <BrowserRouter>
+        <Navbar user={user} setUser={setUser} cart={cart} />
+        <Routes>
+          <Route
+            path="products"
+            element={<ProductsPage user={user} cart={cart} setCart={setCart} />}
+          />
+          <Route path="register" element={<Register />} />
+          <Route path=":itemId" element={<BookPage user={user} cart={cart} setCart={setCart} />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="cart" element={<CartPage cart={cart} setCart={setCart} user={user} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default Main;
+export default Main
