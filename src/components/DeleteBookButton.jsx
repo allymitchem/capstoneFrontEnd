c23d10a5623df6d3b27d4728114d34b4366135b3
Allyson Mitchem <allysonmitchem@gmail.com>
2022-12-01 17:05:32 -0700

    booklist state appears to be updating with delete button in books admin tab

--- src/components/DeleteBookButton.jsx
+++ src/components/DeleteBookButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { deleteBook } from "../api/books";
 import { useNavigate } from "react-router-dom";
+import { updateBookQuantity } from "../api/carts";
 
 const DeleteBookButton = ({book, bookList, setBookList}) => {
     // const navigate = useNavigate()
@@ -11,6 +12,16 @@ const DeleteBookButton = ({book, bookList, setBookList}) => {
         // console.log(event.target.id, "this is todelete")
         const removedBook = await deleteBook(book.id)
         
+        if (removedBook){
+            const updatedBooks = bookList.filter((book) =>{
+                if (book.id == removedBook.id){
+                    return false
+                }
+                return true
+            })
+            console.log(updatedBooks, "updated books")
+            setBookList(updatedBooks)
+        }
         // navigate("/admin")
     }
 
