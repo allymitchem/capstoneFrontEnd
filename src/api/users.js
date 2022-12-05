8d24edcbf888010d75b0f5ddca6f1e7f8a4203d9
tons0trees <forrest@fmcclure.com>
2022-12-04 22:08:54 -0700

    the localCart and userCarts are merging correctly

--- src/api/users.js
+++ src/api/users.js
@@ -1,86 +1,85 @@
-const url = "https://graceshopper-backend.onrender.com/api";
+//const url = 'https://graceshopper-backend.onrender.com/api'
+const url = 'http://localhost:3000/api'
+
 
 function includeToken(paramObj) {
-  const localToken = localStorage.getItem("token");
-  if (localToken) {
-    paramObj.headers["Authorization"] = "Bearer " + localToken;
-  }
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+        paramObj.headers["Authorization"] = "Bearer " + localToken;
+    }
 }
 
 export async function getCurrentUser() {
-  const reqObj = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  includeToken(reqObj);
+    const reqObj = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    includeToken(reqObj);
 
-  
-  try {
+    try {
+        const response = await fetch(url+'/users/me', reqObj)
+        const result = await response.json();
+        return result;
 
-  
-    const response = await fetch(url+'/users/me', reqObj)
-    const result = await response.json();
-    return result;
-
-  } catch (error) {
-    //report an error
-  }
+    } catch (error) {
+        //report an error
+    }
 }
 
 export async function loginUser(username, password) {
-  try {
-    const reqObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    };
-    console.log(reqObj);
-    const response = await fetch(url + "/users/login", reqObj);
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.log("this fetch isn't working");
-  }
+    try {
+        const reqObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        };
+        
+        const response = await fetch(url + "/users/login", reqObj);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log("this fetch isn't working");
+    }
 }
 
 export async function registerUser(username, password, email) {
-  try {
-    const reqObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password, email }),
-    };
-    const response = await fetch(url + "/users/register", reqObj);
-    const result = await response.json();
-    console.log(result, "result in register api");
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const reqObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password, email }),
+        };
+        const response = await fetch(url + "/users/register", reqObj);
+        const result = await response.json();
+        console.log(result, "result in register api");
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function adminUserData(){
-  try {
-    const reqObj ={
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
+    try {
+        const reqObj ={
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+        }
+        includeToken(reqObj)
+        
+        const response = await fetch (url + "/users", reqObj)
+        const result = await response.json()
+     
+        return result
+    }catch(error){
+        console.log(error)
     }
-    }
-    includeToken(reqObj)
-    
-    const response = await fetch (url + "/users", reqObj)
-    const result = await response.json()
-   
-    return result
-  }catch(error){
-    console.log(error)
-  }
 
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,54 +1,58 @@
 import React, { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Navbar, ProductsPage, Register, BookPage, Admin, CartPage, CheckoutConfirmation } from "./"
-import { getActiveCart } from "../api/carts"
+import { getActiveCart, mergeLocalCart } from "../api/carts"
 import { getCurrentUser } from "../api/users"
 
 const Main = () => {
-  const [user, setUser] = useState({ id: 0, username: "guest" })
-  const [cart, setCart] = useState({ id: 0, userId: 0, creatorName: "guest", items: [] })
-  const [count, setCount] = useState(0)
+    const [user, setUser] = useState({ id: 0, username: "guest" })
+    const [cart, setCart] = useState({ id: 0, userId: 0, creatorName: "guest", items: [] })
 
-  useEffect(() => {
+    useEffect(() => {
     //check for token and check that the token belongs to a user
-    const token = localStorage.getItem("token")
-    if (token) {
-      async function callGetCurrentUser() {
-        const currentUser = await getCurrentUser()
+        const token = localStorage.getItem("token")
+        if (token) {
+            async function callGetCurrentUser() {
+                const currentUser = await getCurrentUser()
 
-        if (currentUser) {
-          setUser(currentUser)
-          // setCount(count + 1)
+                if (currentUser) {
+                    setUser(currentUser)
+                }
+            }
+            callGetCurrentUser()
         }
-      }
-      callGetCurrentUser()
-    }
-  }, [])
+    }, [])
 
-  useEffect(() => {
+    useEffect(() => {
     //if a user is logged in then get their cart from DB
 
     setTimeout(() => {
-      if (user.id) {
-        async function callGetCart() {
-          const cartData = await getActiveCart(user.id)
-          if (cartData) {
-            setCart(cartData)
-          }
-        }
-        callGetCart()
-      } else {
-        //since the user is not logged in check if they have a cart in local storage
-        const localCart = localStorage.getItem("cart")
-        if (localCart) {
-          setCart(JSON.parse(localCart))
+        if (user.id) {
+            async function callGetCart() {
+                const userCart = await getActiveCart(user.id)
+                const localCart = JSON.parse(localStorage.getItem("cart"))
+
+                if (userCart) {
+                    if (localCart) {
+                        await mergeLocalCart(userCart,localCart)
+                        localStorage.removeItem("cart")
+                    }
+                    setCart(userCart)
+                }
+            }
+            callGetCart()
         } else {
-          localStorage.removeItem("cart")
-          setCart({ id: 0, userId: 0, creatorName: "guest", items: [] })
+        //since the user is not logged in check if they have a cart in local storage
+            const localCart = localStorage.getItem("cart")
+            if (localCart) {
+                setCart(JSON.parse(localCart))
+            } else {
+                localStorage.removeItem("cart")
+                setCart({ id: 0, userId: 0, creatorName: "guest", items: [] })
+            }
         }
-      }
     }, 250)
-  }, [user])
+}, [user])
 
   return (
     <div id="main">
